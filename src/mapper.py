e4011e3e1537a868d7b12022f32b728dac020b3f
Illuminum2 <trost.zureden08@icloud.com>
2026-08-06 17:00:57 +0200

    [FEAT] Wrap all functionality in separate classes

--- src/mapper.py
+++ src/mapper.py
@@ -6,32 +6,35 @@ from bs4 import BeautifulSoup
 from sites import Site, Page
 
 
-def __crawl_sitemap(sitemap_url: str) -> list[str]:
-    page = requests.get(sitemap_url)
-    xmlSoup = BeautifulSoup(page.content, features="xml")
 
-    url_elements = xmlSoup.find_all('loc') # Extract all link elements from sitemap
-    return [url.text for url in url_elements] # Extract links from all link elements
+class Mapper:
+    @staticmethod
+    def __crawl_sitemap(sitemap_url: str) -> list[str]:
+        page = requests.get(sitemap_url)
+        xmlSoup = BeautifulSoup(page.content, features="xml")
+
+        url_elements = xmlSoup.find_all('loc') # Extract all link elements from sitemap
+        return [url.text for url in url_elements] # Extract links from all link elements
 
 
+    @staticmethod
+    def map_site(base_url: str, sitemap_path="/sitemap.xml") -> Site:
+        sitemap_urls = [base_url + sitemap_path]
+        site = Site(base_url)
 
-def map_site(base_url: str, sitemap_path="/sitemap.xml") -> Site:
-    sitemap_urls = [base_url + sitemap_path]
-    site = Site(base_url)
-
-    xml_prog = re.compile(r"[^?]+\.xml") # Pattern for matching XML sitemaps
-    
-    i = 0
-    while i < len(sitemap_urls):
-        res_urls = __crawl_sitemap(sitemap_urls[i])
-
-        for url in res_urls:
-            if not xml_prog.match(url):
-                if url not in site.page_urls():
-                    site.pages.append(Page(url))
-            elif url not in sitemap_urls:
-                sitemap_urls.append(url)
+        xml_prog = re.compile(r"[^?]+\.xml") # Pattern for matching XML sitemaps
         
-        i += 1
+        i = 0
+        while i < len(sitemap_urls):
+            res_urls = Mapper.__crawl_sitemap(sitemap_urls[i])
 
-    return site
+            for url in res_urls:
+                if not xml_prog.match(url):
+                    if url not in site.page_urls():
+                        site.pages.append(Page(url))
+                elif url not in sitemap_urls:
+                    sitemap_urls.append(url)
+            
+            i += 1
+
+        return site
