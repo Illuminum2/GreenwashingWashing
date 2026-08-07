a3c0c5263018a3b25cbe232c33d34d1cc3b808f5
Illuminum2 <trost.zureden08@icloud.com>
2026-08-07 08:57:09 +0200

    [FEAT] Implement semaphore based concurrency limiting

--- src/crawler.py
+++ src/crawler.py
@@ -1,6 +1,7 @@
 import aiohttp
 import asyncio
 from concurrent.futures import ThreadPoolExecutor
+from typing import Literal
 
 from playwright.async_api import async_playwright, BrowserContext as PlaywrightBrowserContext, TimeoutError as PlaywrightTimeoutError
 from html_to_markdown import convert, ConversionOptions
@@ -11,21 +12,41 @@ from sites import Site, Page
 
 class Crawler:
     @staticmethod
-    async def __fetch_static_html(session: aiohttp.ClientSession, page: Page) -> None:
-        async with session.get(page.url) as response:
-            page.html = await response.text()
+    async def __fetch_static_html(session: aiohttp.ClientSession, page: Page, semaphore: asyncio.Semaphore | None) -> None:
+        try:
+            if semaphore:
+                await semaphore.acquire() # Done manually to allow optional semaphore
+            acquired = True
+        
+            async with session.get(page.url) as response:
+                page.html = await response.text()
+
+        finally:
+            if semaphore and acquired:
+                semaphore.release()
 
 
     @staticmethod
-    async def __fetch_dynamic_html(self, context: PlaywrightBrowserContext, page: Page, max_instances=10) -> None:
-        playwright_page = await context.new_page() # Creating a new page here is faster as page load can be started immediately
-
+    async def __fetch_dynamic_html(context: PlaywrightBrowserContext, page: Page, semaphore: asyncio.Semaphore | None) -> None:
         try:
-            await playwright_page.goto(page.url, wait_until="load", timeout=10000)
-        except PlaywrightTimeoutError:
-            pass # Pass because parts of page might still have loaded
+            if semaphore:
+                await semaphore.acquire() # Done manually to allow optional semaphore
+            acquired = True
 
-        page.html = await playwright_page.content()
+            playwright_page = await context.new_page() # Creating a new page here is faster as page load can be started immediately
+
+            try:
+                await playwright_page.goto(page.url, wait_until="load", timeout=10000)
+            except PlaywrightTimeoutError:
+                pass # Pass because parts of page might still have loaded
+
+            page.html = await playwright_page.content()
+
+            await playwright_page.close()
+
+        finally:
+            if semaphore and acquired:
+                semaphore.release()
 
 
     @staticmethod
@@ -34,10 +55,12 @@ class Crawler:
 
 
     @staticmethod
-    async def crawl_site(site: Site, mode="static") -> None:
+    async def crawl_site(site: Site, mode: Literal['static', 'dynamic'] = "static", concurrent_instances: int | None = 10) -> None:
+        semaphore = asyncio.Semaphore(concurrent_instances) if concurrent_instances else None
+        
         if mode == "static":
             async with aiohttp.ClientSession() as session:
-                tasks = [Crawler.__fetch_static_html(session, page) for page in site.pages]
+                tasks = [Crawler.__fetch_static_html(session, page, semaphore) for page in site.pages]
                 await asyncio.gather(*tasks)
         elif mode == "dynamic":
             async with async_playwright() as playwright:
@@ -46,7 +69,7 @@ class Crawler:
 
                 await context.route("**/*", lambda route: route.abort() if route.request.resource_type in ["image", "stylesheet", "font"] else route.continue_())
 
-                tasks = [Crawler.__fetch_dynamic_html(context, page) for page in site.pages]
+                tasks = [Crawler.__fetch_dynamic_html(context, page, semaphore) for page in site.pages]
                 await asyncio.gather(*tasks)
 
                 await context.close()
