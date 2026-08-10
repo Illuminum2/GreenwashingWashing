49aae72d4865556bb1dc2cbd11c3847d43ae9ed6
Illuminum2 <trost.zureden08@icloud.com>
2026-08-10 16:03:18 +0200

    [FEAT] Implement async static and dynamic network access class

--- src/config.py
+++ src/config.py
@@ -22,6 +22,10 @@ CONCURRENT_WORKER_THREADS = 5 # -1 for unlimited, 0 for no worker threads
 
 CONCURRENT_NETWORK_REQUESTS = 20 # -1 for unlimited, 0 for no network connections
 
+STATIC_SCRAPE_TIMEOUT = 1000 # Time in ms for aiohttp timeout
+
+DYNAMIC_SCRAPE_TIMEOUT = 10000 # Time in ms for playwright timeout
+
 SITEMAP_PATH = "/sitemap.xml"
 
 SCRAPING_MODE = "static" # static/dynamic
---
+++ src/network.py
@@ -0,0 +1,72 @@
+import aiohttp
+import asyncio
+from contextlib import nullcontext
+from typing import Literal
+
+from playwright.async_api import async_playwright, Error as PlaywrightError, TimeoutError as PlaywrightTimeout
+
+from config import CONCURRENT_NETWORK_REQUESTS, SCRAPING_MODE, DYNAMIC_SCRAPE_TIMEOUT, STATIC_SCRAPE_TIMEOUT
+
+
+class Network:
+    _semaphore: asyncio.Semaphore | None = (
+        asyncio.Semaphore(CONCURRENT_NETWORK_REQUESTS)
+        if CONCURRENT_NETWORK_REQUESTS is not None and CONCURRENT_NETWORK_REQUESTS >= 0
+        else None
+    )
+
+
+    def __init__(self, mode: Literal['static', 'dynamic'] = SCRAPING_MODE):
+        self._mode = mode
+        self._session: aiohttp.ClientSession | None = None
+        self._context = None
+        self._playwright, self._browser = None, None
+
+
+    async def fetch_url(self, url: str) -> str | None:
+        # https://stackoverflow.com/a/73556999
+        semaphore = self._semaphore if self._semaphore else nullcontext()
+
+        async with semaphore:
+            try:
+                if self._mode == "static" and self._session:
+                    async with self._session.get(url) as response:
+                        return await response.text()
+                
+                if self._mode == "dynamic" and self._context:
+                    playwright_page = await self._context.new_page() # Creating a new page here is faster as page load can be started immediately
+
+                    try:
+                        try:
+                            await playwright_page.goto(url, wait_until="load", timeout=DYNAMIC_SCRAPE_TIMEOUT)
+                        except PlaywrightTimeout:
+                            print(f"Reached timeout on {url}. Using partial content.")
+                        return await playwright_page.content()
+                    finally:
+                        await playwright_page.close()
+                        
+            except (aiohttp.ClientError, PlaywrightError) as e:
+                print(f"Exception '{type(e)}' was raised while accessing {url}: {e}")
+                return None
+
+
+    async def __aenter__(self):
+        if self._mode == "static":
+            self._session = aiohttp.ClientSession(timeout=aiohttp.ClientTimeout(total=STATIC_SCRAPE_TIMEOUT / 100))
+        else:
+            self._playwright = await async_playwright().start()
+            self._browser = await self._playwright.chromium.launch()
+            self._context = await self._browser.new_context()
+
+        return self
+
+
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
+        if self._session:
+            await self._session.close()
+        if self._context:
+            await self._context.close()
+        if self._browser:
+            await self._browser.close()
+        if self._playwright:
+            await self._playwright.stop()
