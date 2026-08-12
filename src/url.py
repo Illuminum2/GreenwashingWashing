34a576b80f2ffa45428fd2c4d089dbd7ef9c70c0
Illuminum2 <trost.zureden08@icloud.com>
2026-08-12 13:53:25 +0200

    [FEAT] Rewrite url.py for added functionality, usability and stability

--- src/url.py
+++ src/url.py
@@ -1,4 +1,3 @@
-import re
 from urllib.parse import urlparse, urlsplit, urldefrag
 from yarl import URL
 
@@ -8,44 +7,60 @@ from config import BASE_URL
 
 
 class Url:
-    _xml_prog: re.Pattern
-    _base_url_fld: str
+    _base_url_fld: str | None = None
 
 
-    def __init__(self, url: str, base: str | None = None) -> None:
+    def __init__(self, url: str, base: str | None = None, absolute: bool = False) -> None:
         self.raw: str = url
-        self.url = URL(url)
+        self._url = URL(url)
 
         if base is not None:
-            self.setBase(base)
+            self.set_base(base)
+
+        if absolute:
+            self._url = self._url.with_query(None).with_fragment(None)
+
+        self._update_vars(self._url)
 
 
-    def setBase(self, url: str) -> None:
-        base_url= URL(url)
+    def _update_vars(self, url: URL):
+        host: tld.Result = tld.get_tld(url.host, as_object=True, fix_protocol=True, fail_silently=True)
 
-        self.url = URL.build(
-            scheme=base_url.scheme,
-            host=base_url.host,
-            port=base_url.port,
-            path=self.url.path,
-            query=self.url.query,
-            fragment=self.url.fragment
-        )
+        self.subdomain = host.subdomain if host is not None else None
+        self.domain = host.domain if host is not None else None
+        self.tld = host.tld if host is not None else None
+        self.fld = f"{host.domain}.{host.tld}" if host is not None else None
+
+        self.scheme = url.scheme
+        self.host = url.host
+        self.port = url.port
+        self.path = url.path
+        self.query = url.query
+        self.fragment = url.fragment
 
 
-    def isInBase(self) -> bool:
-        if not self._base_url_fld:
+    def set_base(self, base_url: str) -> None:
+        url= URL(base_url)
+
+        self._url = url.join(self._url)
+        self._update_vars(self._url)
+
+
+    @property
+    def is_in_base(self) -> bool:
+        if self._base_url_fld is None:
             try:
                 self._base_url_fld = tld.get_fld(BASE_URL, fix_protocol=True)
             except tld.exceptions.TldDomainNotFound:
                 raise ValueError("Configured base url does not have a valid TLD")
 
-        #return bool(tld.get_fld(url, fix_protocol=True, fail_silently=True) == Matcher._base_url_fld)
         return bool(self.fld == self._base_url_fld)
 
 
-    def isXML(self) -> bool:
-        if not self._xml_prog:
-            self._xml_prog = re.compile(r"[^?#]+\.xml")
+    @property
+    def is_XML(self) -> bool:
+        return bool(self._url.suffix is None or self._url.suffix == ".xml")
 
-        return bool(self._xml_prog.search(self.url))
+
+    def __str__(self):
+        return self._url.human_repr()
