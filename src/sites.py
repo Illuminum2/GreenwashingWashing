121dd9931324d267fbf5fc09305d7070131d67d9
Illuminum2 <trost.zureden08@icloud.com>
2026-08-12 09:31:54 +0200

    [FEAT] Rewrite sites.py for recursive crawling support

--- src/sites.py
+++ src/sites.py
@@ -1,17 +1,35 @@
 from dataclasses import dataclass, field
+from typing import Final
 
 
 @dataclass
 class Site:
-    base_url: str
-    pages: list[Page] = field(default_factory=list)
+    base_url: Final[str]
 
+    _pages: dict[str, Page] = field(default_factory=dict)
+
+    @property
     def page_urls(self) -> list[str]:
-        return [page.url for page in self.pages] 
+        return self._pages.keys
+
+    @property
+    def pages(self) -> list[Page]:
+        return self._page.values
+
+    def add_page(self, url: str) -> Page | None:
+        if url in self.page_urls:
+            return None
+        
+        self._pages[url] = Page(url, self)
+        return self._pages[url]
+
 
 @dataclass
 class Page:
-    url: str
-    raw: str = None
-    content: str = None
+    url: Final[str]
+    site: Site
+
+    raw: str | None = None
+    content: str | None = None
+    links: list[str] = field(default_factory=list)
     matches: list[str] = field(default_factory=list)
