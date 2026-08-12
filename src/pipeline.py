9c0f306c21df3b92390ade655ef0966e91c63110
Illuminum2 <trost.zureden08@icloud.com>
2026-08-12 12:45:34 +0200

    [BUGFIX] Fix pipeline loop

--- src/pipeline.py
+++ src/pipeline.py
@@ -4,7 +4,8 @@ from typing import AsyncIterator, Coroutine, Any
 class Pipeline:
     @staticmethod
     async def queue_drain(q: asyncio.Queue) -> AsyncIterator[Coroutine[Any, Any, Any]]:
-        try:
-            yield await q.get()
-        except asyncio.QueueShutDown:
-            return
+        while True:
+            try:
+                yield await q.get()
+            except asyncio.QueueShutDown:
+                return
