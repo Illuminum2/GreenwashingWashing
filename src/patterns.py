09373e282f59d8755c576e4568352cb8028ba33e
Illuminum2 <trost.zureden08@icloud.com>
2026-08-13 10:40:56 +0200

    [FEAT] Rewrite Patterns for more flexibility

--- src/patterns.py
+++ src/patterns.py
@@ -3,14 +3,19 @@ import re
 
 class Patterns:
     @staticmethod
-    def _compile_patterns(patterns: list[str], flags: re._FlagsType) -> re.Pattern:
-        pattern = '|'.join("(?:%s)" % case for case in patterns) # Merge patterns into one
-        
+    def compile(pattern: str, flags: re._FlagsType = 0) -> re.Pattern:
         if pattern != '': # Empty string matches everything
             return re.compile(pattern, flags)
         return re.compile(r"(?!)") # Pattern matches nothing
 
+
+    @staticmethod
+    def compile_patterns(patterns: list[str], flags: re._FlagsType = 0) -> re.Pattern:
+        pattern = '|'.join("(?:%s)" % case for case in patterns) # Merge patterns into one
+        
+        return Patterns.compile(pattern, flags)
+
     # Case-insensitive, unicode
     @staticmethod
     def compile_patterns_iu(patterns: list[str]) -> re.Pattern:
-        return Patterns._compile_patterns(patterns, re.I | re.U)
+        return Patterns.compile_patterns(patterns, re.I | re.U)
