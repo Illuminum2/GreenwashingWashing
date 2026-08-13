1ac9230f1a06796512a1f9404a9502d3afeb9f46
Illuminum2 <trost.zureden08@icloud.com>
2026-08-13 08:33:30 +0200

    [FEAT] Improve RegEx styling consistency

--- src/matcher.py
+++ src/matcher.py
@@ -11,11 +11,11 @@ from config import MATCH_PATTERNS, MATCH_EXCLUSION_PATTERNS
 class Matcher:
     @staticmethod
     def _compile_patterns(patterns: list[str]) -> re.Pattern:
-        pattern = '|'.join('(?:%s)' % case for case in patterns) # Merge patterns into one
+        pattern = '|'.join("(?:%s)" % case for case in patterns) # Merge patterns into one
 
         if pattern != '': # Empty string matches everything
             return re.compile(pattern, re.I | re.U)
-        return re.compile('?!')
+        return re.compile(r"(?!)")
 
 
     @staticmethod
